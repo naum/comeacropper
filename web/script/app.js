9f92f7153dbf5468c092f66be2b1272c31f36d9e
GNT <aznaum@gmail.com>
2014-12-23 00:08:32 -0700

    initial fetch experimentation

--- web/script/app.js
+++ web/script/app.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  $('#discoveryboard').html('<a href="#">Report</a> data goes in here...');
+  $.getJSON('http://azspot.net/api/read/json/?callback=?', function(d) {
+    console.log(d);
+    $('#discoveryboard').text(d['posts-total']);    
+  });
 });
